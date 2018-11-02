6ed59af330580551416fba7e625a905e96c44e27
shricc <44574606+shricc@users.noreply.github.com>
2018-11-02 15:42:23 +0530

    Add files via upload

---
+++ calc.py
@@ -0,0 +1,28 @@
+while(1):
+    print ("CALCULATOR BOLA TU LUL HAI")
+
+    a=input('Please Enter Your First Number = ')
+    b=input("Please Enter Your Second Number = ")
+    c=input("Choose Your Choice -- + _ * % / // ** -- ")
+    a=int(a)
+    b=int(b)
+    if (c=="a"):
+        print ("Your Result Is1 = ",(a+b))
+    elif (c=='a'):
+        print ("Your Result Is2 = ",(a-b))
+    elif (c=='a'):
+        print ("Your Result Is3 = ",(a*b))
+    elif (c=='a'):
+        print ("Your Result Is4 = ",(a/b))
+    elif (c=='a'):
+        print ("Your Result Is5 = ",(a//b))
+    elif (c=='a'):
+        print ("Your Result Is 6= ",(a**b))
+    elif (c=='a'):
+        print ("Your Result Is 7= ",(a%b))
+    else:
+        print('tu lul hai')
+
+
+
+
